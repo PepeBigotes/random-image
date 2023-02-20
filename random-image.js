d7acd5b3e79dbd6b2cc3226d700a7bf632169313
PepeBigotes <107816872+PepeBigotes@users.noreply.github.com>
2023-02-20 20:40:10 +0100

    Add random-image.js

---
+++ random-image.js
@@ -0,0 +1,13 @@
+const images = [
+    'https://i.imgur.com/MrGY5EL.jpeg',
+    'https://i.imgur.com/Dp2qTdu.jpeg',
+    'https://i.imgur.com/AlBhvW8.jpeg'
+  ];
+  
+  const randomImage = images[Math.floor(Math.random() * images.length)];
+  
+  module.exports = (req, res) => {
+    res.setHeader('Content-Type', 'image/jpeg');
+    res.setHeader('Cache-Control', 'public, max-age=31536000, immutable');
+    res.status(200).send(randomImage);
+  };
