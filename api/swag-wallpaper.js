9569ff0879c19a17961d1d769c1535d5514753cc
PepeBigotes <107816872+PepeBigotes@users.noreply.github.com>
2023-04-10 20:44:12 +0200

    Add swag-wallpapers images list

---
+++ api/swag-wallpaper.js
@@ -0,0 +1,14 @@
+const https = require('https');
+
+const imagesList = require('../lists/swag-wallpapers-list.json');
+const images = imagesList.images;
+  
+  module.exports = (req, res) => {
+    const randomIndex = Math.floor(Math.random() * images.length);
+    const randomImage = images[randomIndex];
+  
+    https.get(randomImage, (response) => {
+      res.setHeader('Content-Type', 'image/jpeg');
+      response.pipe(res);
+    });
+  };
