a85ebd95f5e0f1b1273373c8f772f00bda2de33c
PepeBigotes <107816872+PepeBigotes@users.noreply.github.com>
2023-05-02 19:05:42 +0200

    Add print for selected image for testing purposes

--- api/swag-wallpaper.js
+++ api/swag-wallpaper.js
@@ -6,6 +6,7 @@ const images = imagesList.images;
   module.exports = (req, res) => {
     const randomIndex = Math.floor(Math.random() * images.length);
     const randomImage = images[randomIndex];
+    console.log("Image: " + randomImage);
   
     https.get(randomImage, (response) => {
       res.setHeader('Content-Type', 'image/jpeg');
